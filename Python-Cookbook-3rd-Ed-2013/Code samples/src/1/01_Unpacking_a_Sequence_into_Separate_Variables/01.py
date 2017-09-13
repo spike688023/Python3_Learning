57f45b8013084729ca52acc9bad3d55ae2daf693
abliarsec <jack_6880hotmail.com>
2017-09-13 23:19:33 +0800

    Ch01 question02 Done

---
+++ Python-Cookbook-3rd-Ed-2013/Code samples/src/1/01_Unpacking_a_Sequence_into_Separate_Variables/01.py
@@ -0,0 +1,19 @@
+#You have an N-element tuple or sequence that you would like to unpack into a collection of N variables.
+
+"""
+tuple and sequence(list) 都可給 把變數給 出去，
+
+但前提是接的變數數目要對的起來才可以。
+"""
+list1 = [1, 2, 3]
+a, b, c = list1
+tuple1 =  (4, 5)
+d, e = tuple1
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+
--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # Python3_Learning
 
-這個人的github, 有很多Python相關的書, 
+這個人的github, 有很多Python相關的書,
 
-還有範例, 因為範例直接是答案了, 
+還有範例, 因為範例直接是答案了,
 
 所以我就直接看, 然後把理解的東西, 寫在裡面.
 
 Reference:
 https://github.com/PlamenStilyianov/Python3?files=1
+https://github.com/yidao620c/python3-cookbook
